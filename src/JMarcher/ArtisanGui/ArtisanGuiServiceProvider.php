92384388109e16a6ac5480dd60f8339ff682e763
Joaquin Marcher <joaquin@marcher.com.uy>
2017-02-11 12:10:58 +0100

    First commit

---
+++ src/JMarcher/ArtisanGui/ArtisanGuiServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gordo
+ * Date: 11.02.17
+ * Time: 11:54
+ */
+
+namespace Marcher\ArtisanGui;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class ArtisanGuiServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        // Registers commands (if needed)
+
+        // Needs to register the routes
+    }
+}
